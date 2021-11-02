07cb2ec73215338ebb20ff7a4d16dabbbe6783cb
Chuks Victor <vchuks70@gmail.com>
2021-11-02 19:53:03 +0100

    Add project files.

---
+++ Data/Entity/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+   public class Invoice : BaseClass
+    {
+        public string SellersName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyRegistrationNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+        //public string BuyerName { get; set; }
+        //public string BuyerAddress { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime PaymentDueDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int ReferenceNumber { get; set; }
+    }
+}
---
+++ Data/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Infrastructure
+{
+   
+    public  class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Discounts> Discounts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new InvoiceConfiguration());
+        }
+            
+    }
+}
